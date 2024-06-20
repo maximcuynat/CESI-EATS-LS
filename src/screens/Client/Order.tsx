2f50b4fd38d2e2598c7fbbc4503c91fbc12621ea
Maxim <maxim.cuynat@viacesi.fr>
2024-06-20 20:41:51 +0200

    Zebi bosse un peux non

--- src/screens/Client/Order.tsx
+++ src/screens/Client/Order.tsx
@@ -1,47 +1,85 @@
-import { useEffect } from 'react';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Pressable, View, ScrollView } from 'react-native';
 
-// Api Menu et Article
-import { getMenu } from '../../api/menuAPI';
-import { getArticle } from '../../api/articleAPI';
-// Components
+import ViewDisplay from '../../components/Display';
+import TextView from '../../components/Text';
+import InputView from '../../components/Input';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-import { Text, View } from 'react-native';
-
-interface OrderProps {
-  route: any;
-  navigation: any;
+interface SettingsScreenProps {
+    route: any;
+    navigation: any;
 }
 
-const Order: React.FC<OrderProps> = ({ route, navigation }) => {
-
-  // Récupération des paramètres de la route
-  const { id } = route.params;
-
-  useEffect(() => {
-    // Récupération des données du menu
-    getMenu(id)
-      .then((menu) => {
-        console.log(menu);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-
-    // Récupération des articles
-    getArticle(id)
-      .then((articles) => {
-        console.log(articles);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+const Settings: React.FC<SettingsScreenProps> = ({ route, navigation }) => {
+  
 
   return (
-    <View>
-      <Text>Order</Text>
-    </View>
-  );
-}
+    <SafeAreaView style={styles.safeArea} >
+      <ScrollView style={{flex: 1}}>
 
-export default Order;
+      {/* Header */}
+      <ViewDisplay direction='vertical' align='center' justify='top' type='header'>
+
+      </ViewDisplay>
+        
+        {/* Body */}
+
+        
+      </ScrollView>
+      <Pressable style={styles.headerMenu}>
+        <View>
+          <View>
+            <Pressable>
+              <TextView type='title' style={{width: 'auto', textAlign: 'center',}}>Menus</TextView>
+            </Pressable>
+          </View>
+          <View>
+            <Pressable>
+              <TextView type='title' style={{width: 'auto', textAlign: 'center',}}>Articles</TextView>
+            </Pressable>
+          </View>
+        </View>
+      </Pressable>
+      <Pressable style={styles.btnCart}>
+        <TextView type='title' style={{width: 'auto', textAlign: 'center', color: 'white',}}>Voir mon panier</TextView>
+      </Pressable>
+		</SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+  headerMenu: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    zIndex: 1,
+  },
+
+  btnCart: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
+
+
+    backgroundColor: '#15616D',
+    paddingVertical: 20,
+  },
+
+  // SafeAreaView
+  safeArea:{
+    flex: 1,
+    backgroundColor: '#FFECD1'
+  },
+
+  // Boutton menu
+  buttonAddMenu: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    zIndex: 1,
+    bottom: 0,
+  },
+});
+
+export default Settings;
